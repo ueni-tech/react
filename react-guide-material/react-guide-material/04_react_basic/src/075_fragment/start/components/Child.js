1c25fbe38193ecce24edfc1bcd4eeaa3e059d54a
ueni.tech <ueni.tech@gmail.com>
2023-12-26 21:50:54 +0900

    #1 29

--- react-guide-material/react-guide-material/04_react_basic/src/075_fragment/start/components/Child.js
+++ react-guide-material/react-guide-material/04_react_basic/src/075_fragment/start/components/Child.js
@@ -1,10 +1,15 @@
 import "./Child.css";
+import React from "react";
 
 const Child = () => {
   return (
-    <div className="component">
+    <React.Fragment>
+      <div className="component">
+        <h3>Hello Component</h3>
+      </div>
       <h3>Hello Component</h3>
-    </div>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel culpa dolor saepe sequi natus quidem, repellat omnis, nobis quos minus quaerat expedita quo totam? Iusto culpa eligendi velit vero animi.</p>
+    </React.Fragment >
   );
 };
 
