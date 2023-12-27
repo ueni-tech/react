2ba67932d6e9695e9ab25785ec4e19730e748d04
Kosuke Uenishi <uenishi_kosuke@ca-adv.co.jp>
2023-12-27 17:31:27 +0900

    #1 49

--- react-guide-material/react-guide-material/05_state_and_event/src/055_practice_state/start/Example.js
+++ react-guide-material/react-guide-material/05_state_and_event/src/055_practice_state/start/Example.js
@@ -1,13 +1,20 @@
+import { useState } from 'react';
 const Example = () => {
+  const [count, setCount] = useState(0);
   return (
     <>
-      <h3>練習問題</h3>
-      <p>
-        記述を変更し、完成コードのように+と-ボタンをクリックすると現在のカウント数が1ずつ増減する機能を実装してください。*useStateを用いてcountとsetCountを定義してください。
-      </p>
-      <p>現在のカウント数: {/* ここにcountを表示してください。*/}</p>
-      <button>+</button>
-      <button>-</button>
+      <p>現在のカウント数: {count}</p>
+      <button
+        onClick={
+          () => {
+            setCount(count + 1);
+          }}>+</button>
+      <button
+        onClick={
+          () => {
+            setCount(count - 1);
+          }
+        }>-</button>
     </>
   );
 };
