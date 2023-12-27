fbd4f535e4cfd6458ca8016721729358c8718ee4
Kosuke Uenishi <uenishi_kosuke@ca-adv.co.jp>
2023-12-27 13:29:28 +0900

    #1 37

--- react-guide-material/react-guide-material/04_react_basic/src/110_props_children/start/Example.js
+++ react-guide-material/react-guide-material/04_react_basic/src/110_props_children/start/Example.js
@@ -2,14 +2,17 @@ import Profile from "./components/Profile";
 import Container from "./components/Container";
 
 const profile = [
-  { name: "Takashi", age: 19, country: "Japan", color: "green" },
-  { name: "Jane", age: 28, country: "UK", color: "blue" },
+  { name: "Takashi", age: 19, country: "Japan" },
+  { name: "Jane", age: 28, country: "UK", color: "red" },
 ];
 
 const Example = () => {
   return (
     <div>
-      <Container title="Childrenとは？" />
+      <Container title="Childrenとは？">
+        <Profile {...profile[0]}/>
+        <Profile {...profile[1]}/>
+      </Container>
     </div>
   );
 };
