82b1f1043f59ec507fbc1707c7d6171ce3f80540
Kosuke Uenishi <uenishi_kosuke@ca-adv.co.jp>
2023-12-28 11:59:30 +0900

    #1 59

--- react-guide-material/react-guide-material/06_control_and_form/src/010_list_components/start/Example.js
+++ react-guide-material/react-guide-material/06_control_and_form/src/010_list_components/start/Example.js
@@ -2,12 +2,12 @@
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
-
+  const helloAnimals = animals.map(animal => <li>Hello, {animal}</li>);
   return (
     <>
       <h3>配列の操作</h3>
       <ul>
-        
+        {helloAnimals}
       </ul>
     </>
   );
