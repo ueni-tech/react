1471ca2d43f6782b51127d168c0d26b873a2b888
ueni.tech <ueni.tech@gmail.com>
2024-01-01 12:49:51 +0900

    #1 110

--- react-guide-material/react-guide-material/10_functional_programming/src/030_immutability/start/Example.js
+++ react-guide-material/react-guide-material/10_functional_programming/src/030_immutability/start/Example.js
@@ -1,8 +1,25 @@
 const Example = () => {
+  const num = {val: 2};
+  const double = (num) => {
+    const newNum = {};
+    newNum.val = num.val * 2;
+    return newNum;
+  }
+  const newNum = double(num);
+  console.log('newnum', newNum, 'num', num);
+
+  // const num = 2;
+  //   const double = (num) => {
+  //   return num * 2;
+  // }
+  // const newNum = double(num);
+  // console.log('newnum', newNum, 'num', num);
+
   return (
     <>
       <h3>不変性（Immutability）</h3>
       <p>引数で渡ってきたオブジェクトを変更しない！</p>
+
     </>
   );
 };
