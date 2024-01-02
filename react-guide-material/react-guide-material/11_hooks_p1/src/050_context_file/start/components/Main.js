18d7fb9cf4d14f11cb8f30130d318d9f4555da0c
ueni.tech <ueni.tech@gmail.com>
2024-01-02 11:20:55 +0900

    #1 hooks_1 挫折

--- react-guide-material/react-guide-material/11_hooks_p1/src/050_context_file/start/components/Main.js
+++ react-guide-material/react-guide-material/11_hooks_p1/src/050_context_file/start/components/Main.js
@@ -1,5 +1,14 @@
+import { useContext } from "react";
+import { ThemeContext } from "../Example";
+
 const Main = () => {
-  
+  const [theme] = useContext(ThemeContext);
+
+  return (
+    <main className={`content-${theme}`}>
+      <h1>テーマの切り替え</h1>
+    </main>
+  )
 };
 
 export default Main;
