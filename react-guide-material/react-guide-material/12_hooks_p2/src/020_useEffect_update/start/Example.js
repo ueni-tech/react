0589e3578f8bb9722b81ad6efd2e23ceb8b2712d
ueni.tech <ueni.tech@gmail.com>
2024-01-02 11:52:18 +0900

    #1 127

--- react-guide-material/react-guide-material/12_hooks_p2/src/020_useEffect_update/start/Example.js
+++ react-guide-material/react-guide-material/12_hooks_p2/src/020_useEffect_update/start/Example.js
@@ -9,6 +9,11 @@ const Example = () => {
       setTime(prev => prev + 1);
     }, 1000);
   }, [])
+
+  useEffect(() => {
+    document.title = 'counter: ' + time;
+    window.localStorage.setItem('time-key', time)
+  }, [time])
   
   return (
     <h3>
