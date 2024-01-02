3367aa1e81e9ae0343c77e10ae9ed539471122a8
ueni.tech <ueni.tech@gmail.com>
2024-01-02 14:40:55 +0900

    #1 135

---
+++ react-guide-material/react-guide-material/12_hooks_p2/src/075_practice_customHook/start/Hooks.js
@@ -0,0 +1,12 @@
+import {useState} from 'react';
+
+export const useCount = () => {
+  const [count, setCount] = useState(0);
+
+  const countUp = ()=> setCount(state => state + 1);
+
+  return {
+    count,
+    countUp
+  }
+}
