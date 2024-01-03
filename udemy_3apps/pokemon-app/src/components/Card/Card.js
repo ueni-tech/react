00885f7c88f9a5018ed399dda37c27e4c587d6da
ueni.tech <ueni.tech@gmail.com>
2024-01-03 18:23:25 +0900

    #2 15まで完了

---
+++ udemy_3apps/pokemon-app/src/components/Card/Card.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Card = ({ pokemon }) => {
+  return (
+    <div className='card'>
+      <div className='cardImg'>
+        <img src={pokemon.sprites.front_default} alt="" />
+      </div>
+      <h3 className='cardName'>{pokemon.name}</h3>
+      <div className='cardTypes'>
+        <div>タイプ</div>
+        {pokemon.types.map(type => {
+          return (
+            <div>
+              <span className='typeName'>{type.type.name}</span>
+            </div>
+          )
+        })}
+      </div>
+      <div className='cardInfo'>
+        <div className='cardData'>
+          <p>重さ: {pokemon.weight}</p>
+        </div>
+        <div className='cardData'>
+          <p>高さ: {pokemon.height}</p>
+        </div>
+        <div className='cardData'>
+          <p>アビリティ: {pokemon.abilities[0].ability.name}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
