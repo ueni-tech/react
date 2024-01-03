ad8e262a63116ef4e790b3c419995523f0409d9b
ueni.tech <ueni.tech@gmail.com>
2024-01-03 16:41:08 +0900

    #1 一旦完了

--- react-guide-material/react-guide-material/16_rest_api/src/040_axios_get_request/start/Example.js
+++ react-guide-material/react-guide-material/16_rest_api/src/040_axios_get_request/start/Example.js
@@ -1,4 +1,14 @@
+import {useEffect} from 'react';
+import axios from 'axios';
+
 const Example = () => {
+  useEffect(()=>{
+    const getUser = async () => {
+      const res = await axios.get('http://localhost:3003/user');
+      console.log(res.data);
+    }
+    getUser();
+  });
   return <></>;
 };
 
---
+++ react-guide-material/react-guide-material/16_rest_api/src/060_other_method/start/api/todo.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const ENDPOINT_URL = 'http://localhost:3003/todo';
+
+const todoApi = {
+  async getAll() {
+    const result = await axios.get(ENDPOINT_URL);
+    return result.data;
+  },
+  async post(todo) {
+    const result = await axios.post(ENDPOINT_URL, todo);
+    return result.data;
+  },
+  async delete(todo) {
+    const result = await axios.delete(ENDPOINT_URL + '/' + todo.id);
+    return result.data;
+  },
+  async patch(todo) {
+    const result = await axios.patch(ENDPOINT_URL + '/' + todo.id, todo);
+    return result.data;
+  },
+}
+
+export default todoApi;
