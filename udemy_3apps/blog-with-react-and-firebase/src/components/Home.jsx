40d15d121eb1399d12a4bf158b3eb3abd44f7759
ueni.tech <ueni.tech@gmail.com>
2024-01-04 22:06:46 +0900

    #2 投稿の表示

--- udemy_3apps/blog-with-react-and-firebase/src/components/Home.jsx
+++ udemy_3apps/blog-with-react-and-firebase/src/components/Home.jsx
@@ -1,35 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../firebase';
 
 const Home = () => {
+  const [postList, setPostList] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     const getPosts = async () => {
       const data = await getDocs(collection(db, 'posts'));
-      console.log(data);
-      console.log(data.docs);
-      console.log(data.docs.map(doc => ({doc})));
-      console.log(data.docs.map(doc => ({...doc.data(), id: doc.id})));
+      setPostList(data.docs.map(doc => ({ ...doc.data(), id: doc.id })));
     }
     getPosts();
   }, []);
 
   return (
     <div className='homePage'>
-      <div className="postContents">
-        <div className='postHeader'>
-          <h1>タイトル</h1>
-        </div>
-        <div className="postTextContainer">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi sapiente nisi exercitationem delectus sed, rerum hic illo commodi magnam, alias veniam sunt ipsum. Architecto tenetur expedita, earum iusto veniam voluptatem?
-        </div>
-        <div className="nameAndDeleteButton">
-          <h3>@hogehoge</h3>
-          <button>削除</button>
-        </div>
-      </div>
+      {postList.map((post) => {
+        return (
+          <div className="postContents" key={post.id}>
+            <div className='postHeader'>
+              <h1>{post.title}</h1>
+            </div>
+            <div className="postTextContainer">{post.postText}</div>
+            <div className="nameAndDeleteButton">
+              <h3>@{post.author.username}</h3>
+              <button>削除</button>
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
