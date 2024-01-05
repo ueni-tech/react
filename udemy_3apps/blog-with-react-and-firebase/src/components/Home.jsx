9e4146ade40f882506e69fa2cd1bae7eabc36a8d
Kosuke Uenishi <uenishi_kosuke@ca-adv.co.jp>
2024-01-05 10:13:16 +0900

    #2 blogアプリ完成

--- udemy_3apps/blog-with-react-and-firebase/src/components/Home.jsx
+++ udemy_3apps/blog-with-react-and-firebase/src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './Home.css';
-import { collection, getDocs } from 'firebase/firestore';
-import { db } from '../firebase';
+import { collection, deleteDoc, doc, getDocs } from 'firebase/firestore';
+import { auth, db } from '../firebase';
 
 const Home = () => {
   const [postList, setPostList] = useState([]);
@@ -13,6 +13,11 @@ const Home = () => {
     }
     getPosts();
   }, []);
+  
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, "posts", id))
+    window.location.href = '/';
+  }
 
   return (
     <div className='homePage'>
@@ -25,7 +30,9 @@ const Home = () => {
             <div className="postTextContainer">{post.postText}</div>
             <div className="nameAndDeleteButton">
               <h3>@{post.author.username}</h3>
-              <button>削除</button>
+              {post.author.id === auth.currentUser?.uid && (
+                <button onClick={()=> handleDelete(post.id)}>削除</button>
+              )}
             </div>
           </div>
         )
