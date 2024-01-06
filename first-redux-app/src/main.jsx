6794eac54cc31328a716d4bec86d5b426d83c147
ueni.tech <ueni.tech@gmail.com>
2024-01-06 14:45:59 +0900

    #5 first-redux-app 完了

---
+++ first-redux-app/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createStore } from "redux";
+import allReducers from './reducers/index.js';
+import { Provider } from "react-redux";
+
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+)
