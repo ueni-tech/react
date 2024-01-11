f183043eb1123affe938409e91aa34469348048c
Kosuke Uenishi <uenishi_kosuke@ca-adv.co.jp>
2024-01-11 18:59:10 +0900

    #8 sidebar作成途中

--- discord-clone-udemy/src/components/sidebar/Sidebar.tsx
+++ discord-clone-udemy/src/components/sidebar/Sidebar.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import './Sidebar.scss';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
       {/* sidebarLef */}
-      <div className='sidebarLef'>
-
+      <div className='sidebarLeft'>
+        <div className='serverIcon'>
+          <img src="./logo192.png" alt="" />
+        </div>
+        <div className='serverIcon'>
+          <img src="./logo192.png" alt="" />
+        </div>
       </div>
       {/* sidebarRight */}
       <div className='sidebarRight'>
-        
+        <div className="sidebarTop">
+          <h3>Discord</h3>
+          <ExpandMoreIcon />
+        </div>
+
+        {/* sidebarChannels */}
       </div>
     </div>
   )
