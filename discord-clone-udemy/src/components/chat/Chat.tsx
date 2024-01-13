5c7afbd19ad6468b9a01a71ada955fb5fede1f4d
ueni.tech <ueni.tech@gmail.com>
2024-01-13 16:29:50 +0900

    #8 フロントエンド完了

--- discord-clone-udemy/src/components/chat/Chat.tsx
+++ discord-clone-udemy/src/components/chat/Chat.tsx
@@ -1,12 +1,40 @@
 import React from 'react'
 import './Chat.scss'
+import ChatHeader from './ChatHeader'
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import CardGiftcardIcon from '@mui/icons-material/CardGiftcard';
+import GifIcon from '@mui/icons-material/Gif';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import ChatMessage from './ChatMessage';
 
 const Chat = () => {
   return (
     <div className='chat'>
       {/* chatheader */}
+      <ChatHeader />
       {/* chatMessage */}
+      <div className='chatMessage'>
+        <ChatMessage />
+        <ChatMessage />
+        <ChatMessage />
+        <ChatMessage />
+      </div>
       {/* chatInput */}
+      <div className='chatInput'>
+        <AddCircleOutlineIcon />
+        <form>
+          <input type="text" placeholder='#Udemyへメッセージを送信' />
+          <button type='submit' className='chatInputButton'>
+            送信
+          </button>
+        </form>
+
+        <div className='chatInputIcons'>
+          <CardGiftcardIcon />
+          <GifIcon />
+          <EmojiEmotionsIcon />
+        </div>
+      </div>
     </div>
   )
 }
