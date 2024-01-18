a5e56e6dccd34e4b2946ff8527df4c660d725995
Kosuke Uenishi <uenishi_kosuke@ca-adv.co.jp>
2024-01-18 11:08:37 +0900

    #10 公式のExampleをコピー

--- gantt-app/src/index.tsx
+++ gantt-app/src/index.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import "./index.css";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(<App />);
