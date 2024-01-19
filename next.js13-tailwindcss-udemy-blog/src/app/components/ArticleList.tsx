80deb66691e798f60e1e94e2f7b9eca56883cd5d
Kosuke Uenishi <uenishi_kosuke@ca-adv.co.jp>
2024-01-19 11:34:42 +0900

    #11 html作成　完了

--- next.js13-tailwindcss-udemy-blog/src/app/components/ArticleList.tsx
+++ next.js13-tailwindcss-udemy-blog/src/app/components/ArticleList.tsx
@@ -5,24 +5,46 @@ import React from 'react'
 const ArticleList = () => {
   return (
     <div>
-      <article>
-        <Link href="#">
+      <article className='shadow my-4 flex flex-col'>
+        <Link href="#" className='hover:opacity-75'>
           <Image
           src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
           alt=""
           width={1280}
           height={300} />
         </Link>
-        <div>
-          <Link href="#">Technology</Link>
-          <Link href="#">
+        <div className='bg-white flex flex-col justify-start p-6'>
+          <Link href="#" className='text-blue-700 pb-4 font-bold'>Technology</Link>
+          <Link href="#" className='text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4'>
             Next.jsの勉強中
           </Link>
-          <p>By. ueniTech, Published on 2023/07/15</p>
-          <Link href="#">
+          <p className='text-sm pb-3 text-slate-900'>Published on 2023/07/15</p>
+          <Link href="#" className='text-slate-900 pb-6'>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis quas esse, placeat tenetur, explicabo ducimus molestias quos dicta enim quam quo aperiam corrupti quod, facere nulla sit autem mollitia provident!
           </Link>
-          <Link href="#">
+          <Link href="#" className='text-pink-800 hover:text-black'>
+            続きを読む
+          </Link>
+        </div>
+      </article>
+      <article className='shadow my-4 flex flex-col'>
+        <Link href="#" className='hover:opacity-75'>
+          <Image
+          src="https://source.unsplash.com/collection/1346951/1000x500?sig=2"
+          alt=""
+          width={1280}
+          height={300} />
+        </Link>
+        <div className='bg-white flex flex-col justify-start p-6'>
+          <Link href="#" className='text-blue-700 pb-4 font-bold'>Technology</Link>
+          <Link href="#" className='text-slate-900 text-3xl font-bold hover:text-gray-700 pb-4'>
+            Next.jsの勉強中
+          </Link>
+          <p className='text-sm pb-3 text-slate-900'>Published on 2023/07/15</p>
+          <Link href="#" className='text-slate-900 pb-6'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis quas esse, placeat tenetur, explicabo ducimus molestias quos dicta enim quam quo aperiam corrupti quod, facere nulla sit autem mollitia provident!
+          </Link>
+          <Link href="#" className='text-pink-800 hover:text-black'>
             続きを読む
           </Link>
         </div>
