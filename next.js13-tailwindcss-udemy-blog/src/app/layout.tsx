a9904daa41f01e96deb4e6053749056af42272fd
Kosuke Uenishi <uenishi_kosuke@ca-adv.co.jp>
2024-01-19 15:05:28 +0900

    #11 記事詳細　作成開始

--- next.js13-tailwindcss-udemy-blog/src/app/layout.tsx
+++ next.js13-tailwindcss-udemy-blog/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 import Header from './Header'
 import Footer from './Footer'
+import { Suspense } from 'react'
+import Loading from './loading'
 
 
 export const metadata: Metadata = {
@@ -18,7 +20,9 @@ export default function RootLayout({
     <html lang="ja">
       <body className='container mx-auto bg-slate-700 text-slate-50'>
         <Header />
-        {children}
+        <Suspense fallback={<Loading />} >
+          {children}
+        </Suspense>
         <Footer />
       </body>
     </html>
--- next.js13-tailwindcss-udemy-blog/src/blogAPI.ts
+++ next.js13-tailwindcss-udemy-blog/src/blogAPI.ts
@@ -3,6 +3,12 @@ import { Article } from "./types";
 export const getAllArticles = async(): Promise<Article[]> => {
   const res = await fetch(`http://localhost:3001/posts`, {cache: "no-store"}); // SSR
 
+  if(!res.ok){
+    throw new Error("エラーが発生しました");
+  }
+
+  // await new Promise(resolve => setTimeout(resolve, 800)); 
+
   const articles = await res.json();
   return articles;
 }
